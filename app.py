ae43d91c0a348400a3415ed5038c86c06870684f
Davut Özdemir <davut.oezdemir@gmx.de>
2025-11-20 15:18:34 +0100

    Neue Datensrtuktur erstellt

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/')
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
